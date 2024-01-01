81e07233fd8ea9c0f14f4b1b5fb8c45c0f3f393f
Eugene <fabasoad@gmail.com>
2024-01-01 15:08:05 +0900

    Create basic functionality (#1)

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# reviewers-action
-A GitHub action to parse a CODEOWNERS like files and use it to set reviewers on PRs
+# Reviewers action
+
+[![Stand With Ukraine](https://raw.githubusercontent.com/vshymanskyy/StandWithUkraine/main/badges/StandWithUkraine.svg)](https://stand-with-ukraine.pp.ua)
+![GitHub release](https://img.shields.io/github/v/release/fabasoad/reviewers-action?include_prereleases)
+![linting](https://github.com/fabasoad/reviewers-action/actions/workflows/linting.yml/badge.svg)
+
+This GitHub action to parse a CODEOWNERS like files and use it to set reviewers
+on PRs.
+
+## Prerequisites
+
+The following tools have to be installed for successful work of this GitHub action:
+`bash`.
+
+## Permissions
+
+None
+
+## Inputs
+
+```yaml
+with:
+  # (Optional) Auth token with permissions to assign reviewers to a PR. Defaults
+  # to ${{ github.token }}.
+  github-token: "${{ secrets.GITHUB_TOKEN }}"
+  # (Optional) CODEOWNERS like filename. Defaults to "REVIEWERS".
+  reviewers-filename: "REVIEWERS"
+```
+
+## Outputs
+
+None.
---
+++ src/get-reviewers.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+reviewers_filename="$1"
+changed_files_list="$2"
+
+build_reviewers_str() {
+  local -n data_ref=$1
+  n=0
+  reviewers_str=""
+  for github_slug in "${!data_ref[@]}"
+  do
+    n=$((n + 1))
+    if [ $n -gt 1 ]; then
+      reviewers_str="${reviewers_str},"
+    fi
+    if [[ $github_slug == "@"* ]]; then
+      github_slug="${github_slug:1}"
+    fi
+    reviewers_str="${reviewers_str}\"${github_slug}\""
+  done
+  echo "${reviewers_str}"
+}
+
+# Prepare hash maps with the reviewers list. Both IC and team handles.
+declare -A reviewers_ic_map
+declare -A reviewers_team_map
+base_list=$(codeowners audit -c "${reviewers_filename}")
+for changed_file in ${changed_files_list}; do
+  n=0
+  for github_slug in $(echo "${base_list}" | grep "${changed_file}")
+  do
+    n=$((n + 1))
+    if [ $n -lt 2 ]; then
+      continue
+    fi
+    if [[ $github_slug == *"/"* ]]; then
+      # shellcheck disable=SC2034
+      reviewers_team_map[$github_slug]=1
+    else
+      # shellcheck disable=SC2034
+      reviewers_ic_map[$github_slug]=1
+    fi
+  done
+done
+
+# Comma-separated list with IC reviewers
+reviewers_ic_str=$(build_reviewers_str reviewers_ic_map)
+# Comma-separated list with team reviewers
+reviewers_team_str=$(build_reviewers_str reviewers_team_map)
+
+json="{\"reviewers\":[${reviewers_ic_str}],\"teamReviewers\":[${reviewers_team_str}]}"
+echo "json=${json}" >> "$GITHUB_OUTPUT"

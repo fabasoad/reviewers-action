b46ab8f268fa25819ae96b9630a4419ee7afb510
fabasoad <fabasoad@gmail.com>
2024-01-02 13:55:11 +0900

    Fix script

--- src/get-changed-files.sh
+++ src/get-changed-files.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env sh
 
-base_ref="$1"
-head_ref="$2"
+adjust_branch_name() {
+  original_name="$1"
+  prefix="origin/"
+
+  case "${original_name}" in
+    "${prefix}"*) echo "${original_name}";;
+    *) echo "${prefix}${original_name}";;
+  esac
+}
+
+base_ref="$(adjust_branch_name "$1")"
+head_ref="$(adjust_branch_name "$2")"
 
 changed_files=$(git diff --name-only "${base_ref}" "${head_ref}")
 echo "all=${changed_files}" >> "$GITHUB_OUTPUT"
